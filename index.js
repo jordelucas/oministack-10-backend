635ec767238acdef4553576c8a25bea84102f270
jordelucas <jordevalucas@hotmail.com>
2020-01-14 23:15:06 -0300

    Apresentacao dos metodos HTTP e parametros
    Os testes vêm sendo feitos através do software Insominia

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello Dev' })
+app.use(express.json())
+
+/* Métodos HTTP */
+
+// get - buscar
+// post - criar
+// put - atualizar/editar
+// delete - deletar
+
+// Tipos:
+
+// Query Params - request.query -> Filtros, ordenacao, paginacao 
+// Route Params - request.params -> Identificar um recurso na alteracao ou remocao
+// Body - request.body -> Dados para criacao ou alteracao de um registro
+
+
+app.post('/users/:id', (request, response) => {
+    console.log(request.body)
+    return response.json({ message: 'Hello Omnistack' })
 })
 
 app.listen(3333)
