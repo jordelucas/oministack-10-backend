e239fa5eec138f15675b73fc0abd116aecc1393b
jordelucas <jordevalucas@hotmail.com>
2020-01-15 16:45:53 -0300

    armazena usuario no BD

--- src/models/Dev.js
+++ src/models/Dev.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const DevSchema = new mongoose.Schema({
     name: String,
     github_username: String,
-    biografia: String,
+    bio: String,
     avatar_url: String,
-    techs: [String]
-})
+    techs: [String],
+});
 
 module.exports = mongoose.model('Dev', DevSchema)
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,27 @@
 const { Router } = require('express');
 const axios = require('axios')
+const Dev = require('./models/Dev')
 
 const routes = Router();
 
 routes.post('/devs', async (request, response) => {
     const { github_username, techs } = request.body;
-
+    
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
 
     const { name = login, avatar_url, bio } = apiResponse.data
 
-    const techsArray = techs.split(',').map(tech => trim)
+    const techsArray = techs.split(',').map(tech => tech.trim())
 
-    return response.json({ message: 'Hello Omnistack' })
+    const dev = await Dev.create({
+        name,
+        github_username,
+        bio,
+        avatar_url,
+        techs: techsArray,
+    })
+
+    return response.json(dev);
 })
 
 module.exports = routes;
