4ed5f764c6961f728d2c3b41466e91b14d9299b8
jordelucas <jordevalucas@hotmail.com>
2020-01-15 15:13:41 -0300

    especifica quais dados do github devem ser guardados

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ const axios = require('axios')
 const routes = Router();
 
 routes.post('/devs', async (request, response) => {
-    const { github_username } = request.body;
+    const { github_username, techs } = request.body;
 
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
 
-    console.log(apiResponse.data)
+    const { name = login, avatar_url, bio } = apiResponse.data
+
+    const techsArray = techs.split(',').map(tech => trim)
 
     return response.json({ message: 'Hello Omnistack' })
 })
