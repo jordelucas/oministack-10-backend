6200b0c25944f76f96fe12c0666c77b9ad248856
jordelucas <jordevalucas@hotmail.com>
2020-01-15 16:58:50 -0300

    adiciona dados de latitude e longitude

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const Dev = require('./models/Dev')
 const routes = Router();
 
 routes.post('/devs', async (request, response) => {
-    const { github_username, techs } = request.body;
+    const { github_username, techs, latitude, longitude } = request.body;
     
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
 
@@ -13,12 +13,18 @@ routes.post('/devs', async (request, response) => {
 
     const techsArray = techs.split(',').map(tech => tech.trim())
 
+    const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+    }
+
     const dev = await Dev.create({
         name,
         github_username,
         bio,
         avatar_url,
         techs: techsArray,
+        location,
     })
 
     return response.json(dev);
